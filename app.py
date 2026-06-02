8d1d703be7c2edd026aa3a4d607de8fecb6e72c4
Mario <mario@ejemplo.com>
2026-06-01 22:56:40 -0600

    First commit

--- app.py
+++ app.py
@@ -1095,4 +1095,4 @@ def sync_vehicles():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False)
+    socketio.run(app, host='0.0.0.0', port=port, debug=True, allow_unsafe_werkzeug=True)
